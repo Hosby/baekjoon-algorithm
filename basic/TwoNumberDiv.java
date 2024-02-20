4c66d652b9f314fc0648438d08d90a4c0889069d
hykim <hoyun95@naver.com>
2024-02-20 22:41:38 +0900

    fix: 문제, 입출력 추가

--- basic/TwoNumberDiv.java
+++ basic/TwoNumberDiv.java
@@ -4,6 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+// 문제 : 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+// 입력 : 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+// 출력 : 첫째 줄에 A/B를 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10^9 이하이면 정답이다.
+// 예제 입력1 : 1 3
+// 예제 출력1 : 0.33333333333333333333333333333333
+// 예제 입력1 : 4 5
+// 예제 출력1 : 0.8
 
 public class TwoNumberDiv {
     // BufferedReader 사용 풀이법
